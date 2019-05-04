02950b7aeb61fbbd418e73ada2a96b48ec47f07a
wushaolin <15265249516@163.com>
2019-05-04 16:00:22 +0800

    What: :smile: add parcel index.html
    Why:
    
    * 我为什么要这么做？
    
    How:
    
    * 我是怎么做的？这么做会有什么副作用？

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="w-button">按钮</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: "w-button"
+  };
+</script>
+<style lang="scss">
+  .w-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      display: none;
+    }
+  }
+</style>
