7b96c5e029b258ceeab857c68cf7f41e9368d5bd
wushaolin <15265249516@163.com>
2019-05-04 19:46:58 +0800

    What::zap: 使用Icon.vue实现完全组件化
    Why:
    
    * 我为什么要这么做？
    
    How:
    
    * 我是怎么做的？这么做会有什么副作用？

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import Button from './button.vue'
+import Button from './Button.vue'
+import Icon from './Icon.vue'
 
 Vue.component('w-button', Button)
+Vue.component('w-icon', Icon)
 
 new Vue({
   el: '#app'
