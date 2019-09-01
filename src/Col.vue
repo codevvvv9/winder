25df3db8d945c5ba56c4445acb9589023db45a54
wushaolin <15265249516@163.com>
2019-09-01 15:38:35 +0800

    What::recycle:  refactor Row.vue and Col.vue
    Why:
    
    *
    
    How:
    
    * use computed instead of style-inline

--- src/Col.vue
+++ src/Col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
-    <div style="border: 1px solid green;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 <script>
@@ -27,31 +24,49 @@
         gutter: 0
       }
     },
+    computed: {
+      //计算span
+      colClass() {
+        let {
+          span,
+          offset
+        } = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      //计算gutter
+      colStyle() {
+        return {
+          "padding-left": this.gutter / 2 + 'px',
+          "padding-right": this.gutter / 2 + 'px'
+        }
+      }
+    },
   }
 </script>
 <style lang="scss" scoped>
   .col {
     height: 100px;
     width: 50%;
-    // border: 1px solid red;
-    // background-color: #666666;
     //grid每一行的布局所占百分比
     $class-prefix: col-;
+
     // loops through 100 times
     @for $n from 1 through 24 {
+
       // for each $col_#{n}
       &.#{$class-prefix}#{$n} {
-        // set the background-color to cornflowerblue and
-        // each time lighten the color slightly
         width: ($n / 24) * 100%;
       }
     }
+
     //grid每一行之间的偏移量
     $class-prefix: offset-;
+
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        // set the background-color to cornflowerblue and
-        // each time lighten the color slightly
         margin-left: ($n / 24) * 100%;
       }
     }
--- src/Row.vue
+++ src/Row.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
 <script>
-export default {
-  props: {
-    gutter: {
-      type: [Number, String]
-    }
-  },
-  created() {
-    console.log('children', this.$children)
-  },
-  mounted() {
-    console.log('children', this.$children)
-    this.$children.forEach(vm => {
-      vm.gutter = this.gutter
-    });
-  },
-}
+  export default {
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted() {
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      });
+    },
+    computed: {
+      //计算row的gutter
+      rowStyle() {
+        let {gutter} = this
+        return {
+          "margin-left": -gutter / 2 + 'px',
+          "margin-right": -gutter / 2 + 'px'
+        }
+      }
+    },
+  }
 </script>
 <style lang="scss" scoped>
   .row {
