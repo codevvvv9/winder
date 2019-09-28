8651bac2186197b0b98992622791025262b51aa7
wushaolin <15265249516@163.com>
2019-09-28 23:29:56 +0800

    What::zap: complete dafault Layout
    Why:
    
    *
    
    How:
    
    *

--- src/Layout.vue
+++ src/Layout.vue
@@ -23,9 +23,10 @@ export default {
 </script>
 <style lang="scss" scoped>
   .layout {
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
-    border: 1px solid red;
+    // border: 1px solid red;
     &.hasSider {
       flex-direction: row;
     }
--- src/sider.vue
+++ src/sider.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="handleClose">close</button>
+    </div>
+  </transition>
 </template>
 <script>
   export default {
-    name: "WinderSider"
+    name: "WinderSider",
+    data() {
+      return {
+        visible: true
+      }
+    },
+    methods: {
+      handleClose() {
+        this.visible = !this.visible
+      }
+    },
   }
 </script>
 <style lang="scss" scoped>
-  .sider {}
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s; 
+  }
+  .slide-enter, .slide-leave-to {
+    margin-left: -200px; 
+  }
+
+  .sider {
+    position: relative;
+
+    >button {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
 </style>
