bf563ddcfd58b33577afb3217eec4dd2f4b92b90
wushaolin <15265249516@163.com>
2019-11-02 16:43:43 +0800

    What: optimize Toast.vue use plugin.js
    Why:
    
    *
    
    How:
    
    *

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="toast">
-    
+    <slot></slot>
   </div>
 </template>
 <script>
 export default {
   name: "WinderToast",
-  created() {
-  },
-}
+  created() {}
+};
 </script>
 <style lang="scss">
-  .toast {
-
-  }
+$font-size: 14px;
+$line-height: 1.8;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.74);
+.toast {
+  position: fixed;
+  height: $toast-height;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  line-height: $line-height;
+  display: flex;
+  align-items: center;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 0 16px;
+}
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,14 @@
+import Toast from './Toast.vue'
+
 export default {
   install(Vue, options) {
     Vue.prototype.$toast = function (message) {
-      alert(message)
+      let Constructor = Vue.extend(Toast)
+      let toast = new Constructor()
+      toast.$slots.default = [message] // default是一个数组
+      toast.$mount()
+      document.body.appendChild(toast.$el)
+
     }
   }
 }
