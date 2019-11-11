761dd38a43be3bfd5b7a4b1bc3b1232d430906f0
wushaolin <15265249516@163.com>
2019-11-11 23:41:26 +0800

    What: optimize Toast.vue
    Why:
    
    *
    
    How:
    
    *

--- src/app.js
+++ src/app.js
@@ -1,58 +1,61 @@
-import Vue from 'vue'
-import Button from './Button.vue'
-import Icon from './Icon.vue'
-import ButtonGroup from './ButtonGroup.vue'
-import Input from './Input.vue'
-import Row from './Row.vue'
-import Col from './Col.vue'
-import Layout from './Layout.vue'
-import Header from './Header.vue'
-import Content from './Content.vue'
-import Footer from './Footer.vue'
-import Sider from './Sider.vue'
-import Toast from './Toast.vue'
-import plugin from './plugin'
+import Vue from "vue";
+import Button from "./Button.vue";
+import Icon from "./Icon.vue";
+import ButtonGroup from "./ButtonGroup.vue";
+import Input from "./Input.vue";
+import Row from "./Row.vue";
+import Col from "./Col.vue";
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Content from "./Content.vue";
+import Footer from "./Footer.vue";
+import Sider from "./Sider.vue";
+import Toast from "./Toast.vue";
+import plugin from "./plugin";
 
-Vue.component('w-button', Button)
-Vue.component('w-icon', Icon)
-Vue.component('w-button-group', ButtonGroup)
-Vue.component("w-input", Input)
-Vue.component("w-row", Row)
-Vue.component("w-col", Col)
-Vue.component("w-layout", Layout)
-Vue.component("w-header", Header)
-Vue.component("w-content", Content)
-Vue.component("w-footer", Footer)
-Vue.component("w-sider", Sider)
-Vue.component("w-toast", Toast)
-Vue.use(plugin)
+Vue.component("w-button", Button);
+Vue.component("w-icon", Icon);
+Vue.component("w-button-group", ButtonGroup);
+Vue.component("w-input", Input);
+Vue.component("w-row", Row);
+Vue.component("w-col", Col);
+Vue.component("w-layout", Layout);
+Vue.component("w-header", Header);
+Vue.component("w-content", Content);
+Vue.component("w-footer", Footer);
+Vue.component("w-sider", Sider);
+Vue.component("w-toast", Toast);
+Vue.use(plugin);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   data() {
     return {
       isLoading: false,
-      message: 'hi, v-model'
-    }
+      message: "hi, v-model"
+    };
   },
   created() {},
   methods: {
     changeMessage() {
-      this.message += '!'
+      this.message += "!";
     },
     showToast() {
-      this.$toast('<p><strong>hi </strong>你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签<p>', {
-        closeButton: {
-          text: "关闭",
-          callback(toast) {
-            toast.log()
-            console.log('关闭被触发了');
-          }
-        },
-        enableHtml: true,
-        autoClose: false,
-        position: "top"
-      })
+      this.$toast(
+        "<p><strong>hi </strong>你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签你好好我是一个html标签<p>",
+        {
+          closeButton: {
+            text: "关闭",
+            callback(toast) {
+              toast.log();
+              console.log("关闭被触发了");
+            }
+          },
+          enableHtml: true,
+          autoClose: false,
+          position: "middle"
+        }
+      );
     }
-  },
-})
+  }
+});
--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,28 @@
-import Toast from './Toast.vue'
+import Toast from "./Toast.vue";
 
+let currentToast
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = function (message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message] // default是一个数组
-      toast.$mount()
-      document.body.appendChild(toast.$el)
-
-    }
+    Vue.prototype.$toast = function(message, toastOptions) {
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, message, propsData: toastOptions})
+    };
   }
+};
+
+/**
+ * create a Toast instance
+ * @param { Object } param0 
+ */
+function createToast({ Vue, message, propsData }) {
+  let Constructor = Vue.extend(Toast);
+  let toast = new Constructor({
+    propsData
+  });
+  toast.$slots.default = [message]; // default是一个数组
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+  return toast
 }
