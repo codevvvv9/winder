92597e5d0d5331e95626f16c7ab6c35d3b7550bd
wushaolin <15265249516@163.com>
2019-11-30 13:52:26 +0800

    What: refactor Popover.vue
    Why:
    
    *
    
    How:
    
    *

--- src/PopOver.vue
+++ src/PopOver.vue
@@ -18,52 +18,42 @@
       }
     },
     methods: {
+      //添加contentWrapper到body,并定位
+      setContentWrapperPosition() {
+        let contentWrapperEl = this.$refs.contentWrapper
+        document.body.appendChild(contentWrapperEl)
+        let {
+          width,
+          height,
+          top,
+          left
+        } = this.$refs.slotWrapper.getBoundingClientRect()
+        contentWrapperEl.style.left = `${left+window.scrollX}px`
+        contentWrapperEl.style.top = `${top + window.scrollY}px`
+      },
+      //关闭contentWrapper
+      closeContentWrapper() {
+        let eventHandler = (e) => {
+          if (this.$refs.contentWrapper && !this.$refs.contentWrapper.contains(e.target)) {
+            //点击的是内容区域不想关闭弹框
+            console.log('button click事件关闭');
+            this.contentVisible = false
+            document.removeEventListener("click", eventHandler)
+          }
+        }
+        document.addEventListener("click", eventHandler)
+      },
       popoverClick(event) {
-        console.log('clickTarget', event.target);
         let clickTarget = event.target
         if (this.$refs.slotWrapper.contains(clickTarget)) {
           this.contentVisible = !this.contentVisible
           if (this.contentVisible === true) {
             this.$nextTick(() => {
-              let contentWrapperEl = this.$refs.contentWrapper
-              document.body.appendChild(contentWrapperEl)
-              let { width, height, top, left } = this.$refs.slotWrapper.getBoundingClientRect()
-              console.log('top', top);
-              contentWrapperEl.style.left = `${left+window.scrollX}px`
-              contentWrapperEl.style.top = `${top + window.scrollY}px`
-              let eventHandler = (e) => {
-                if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target)) {
-                  //点击的是内容区域不想关闭弹框
-                  return 
-                } else {
-                  console.log('button click事件关闭');
-                  this.contentVisible = false
-                  document.removeEventListener("click", eventHandler)
-                }
-              }
-              document.addEventListener("click", eventHandler)
+              this.setContentWrapperPosition()
+              this.closeContentWrapper()
             })
           }
         }
-        // this.contentVisible = !this.contentVisible
-        // if (this.contentVisible === true) {
-        //   this.$nextTick(() => {
-        //     let contentWrapperEl = this.$refs.contentWrapper
-        //     document.body.appendChild(contentWrapperEl)
-        //     let { width, height, top, left } = this.$refs.slotWrapper.getBoundingClientRect()
-        //     console.log('top', top);
-        //     contentWrapperEl.style.left = `${left+window.scrollX}px`
-        //     contentWrapperEl.style.top = `${top + window.scrollY}px`
-        //     let eventHandler = () => {
-        //       console.log('button click事件关闭');
-        //       this.contentVisible = false
-        //       document.removeEventListener("click", eventHandler)
-        //     }
-        //     document.addEventListener("click", eventHandler)
-        //   })
-        // } else {
-        //   console.log('vm 隐藏content');
-        // }
       }
     },
   }
