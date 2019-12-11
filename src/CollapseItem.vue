f79f739a5eb17f7d38083efbdfcd647b5e1bd0af
wushaolin <15265249516@163.com>
2019-12-11 22:50:14 +0800

    What: Set default selection in Collapse.vue
    Why:
    
    *
    
    How:
    
    *

--- src/CollapseItem.vue
+++ src/CollapseItem.vue
@@ -15,6 +15,10 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
   },
   inject: ['eventBus'],
@@ -30,9 +34,11 @@ export default {
 
   },
   mounted() {
-    this.eventBus && this.eventBus.$on("update:selected", (vm) => {
-      if (vm !== this) {
+    this.eventBus && this.eventBus.$on("update:selected", (name) => {
+      if (name !== this.name) {
         this.close()
+      } else {
+        this.show()
       }
     })
   },
@@ -44,12 +50,14 @@ export default {
       if (this.open) {
         this.open = false
       } else {
-        this.open = true
-        this.eventBus && this.eventBus.$emit("update:selected", this)
+        this.eventBus && this.eventBus.$emit("update:selected", this.name)
       }
     },
     close() {
       this.open = false
+    },
+    show() {
+      this.open = true
     }
   },
   components: {
