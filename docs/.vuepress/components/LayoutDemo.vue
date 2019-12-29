477131525d07c74cf58caed8b627c8f3bd412a06
wushaolin <15265249516@163.com>
2019-12-29 22:04:01 +0800

    What: add Layout component in vuepress
    Why:
    
    *
    
    How:
    
    *

---
+++ docs/.vuepress/components/LayoutDemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <w-layout>
+      <w-header>header</w-header>
+      <w-content>content</w-content>
+      <w-footer>footer</w-footer>
+    </w-layout>
+  </div>
+</template>
+
+<script>
+import Layout from '../../../src/Layout'
+import Header from '../../../src/Header'
+import Content from '../../../src/Content'
+import Footer from '../../../src/Footer'
+import Sider from '../../../src/Sider'
+export default {
+  components: {
+    "w-layout": Layout,
+    "w-header": Header,
+    "w-content": Content,
+    "w-footer": Footer,
+    "w-sider": Sider
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- docs/components/layout.md
+++ docs/components/layout.md
@@ -3,3 +3,17 @@ title: Layout
 ---
 
 # Layout
+## Basic Usage
+This is the basic usage of the Layout component, it contain four parts-header,content,footer or sider.
+
+<ClientOnly>
+  <LayoutDemo></LayoutDemo>
+</ClientOnly>
+
+``` html
+<w-layout>
+  <w-header>header</w-header>
+  <w-content>content</w-content>
+  <w-footer>footer</w-footer>
+</w-layout>
+```
