c38c390724725822593e918046ce81bc095b11df
wushaolin <15265249516@163.com>
2020-01-05 22:06:19 +0800

    What: update Tabs in vuepress
    Why:
    
    *
    
    How:
    
    *

--- docs/.vuepress/components/Tabs.vue
+++ docs/.vuepress/components/Tabs.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <w-tab selected="item2" direction="vertical">
-      <w-tab-item name="item1">item1</w-tab-item>
-      <w-tab-item name="item2">item2</w-tab-item>
-      <w-tab-item name="item3">item3</w-tab-item>
-    </w-tab>
+    <w-tabs selected="finance">
+      <w-tabs-head>
+        <w-tabs-item name="sports"> 体育 </w-tabs-item>
+        <w-tabs-item name="finance"> 经济</w-tabs-item>
+        <w-tabs-item name="history"> 历史 </w-tabs-item>
+      </w-tabs-head>
+      <w-tabs-body>
+        <w-tabs-pane name="sports"> 体育新闻 </w-tabs-pane>
+        <w-tabs-pane name="finance"> 财经新闻 </w-tabs-pane>
+        <w-tabs-pane name="history"> 历史趣事 </w-tabs-pane>
+      </w-tabs-body>
+    </w-tabs>
   </div>
 </template>
 
 <script>
-import Tabs from '../../../src/Tabs'
-import TabsItem from '../../../src/TabsItem'
-export default {
-  components: {
-    "w-tab": Tabs,
-    "w-tab-item": TabsItem
-  },
-};
+  import Tabs from '../../../src/Tabs'
+  import TabsItem from '../../../src/TabsItem'
+  import TabsHead from '../../../src/TabsHead.vue'
+  import TabsBody from '../../../src/TabsBody.vue'
+  import TabsPane from '../../../src/TabsPane.vue'
+  export default {
+    components: {
+      "w-tabs": Tabs,
+      "w-tabs-item": TabsItem,
+      "w-tabs-head": TabsHead,
+      "w-tabs-body": TabsBody,
+      "w-tabs-pane": TabsPane
+    },
+  };
 </script>
 
 <style>
--- docs/components/tabs.md
+++ docs/components/tabs.md
@@ -9,3 +9,18 @@ This is the basic usage of the Tabs component.
 <ClientOnly>
   <Tabs></Tabs>
 </ClientOnly>
+
+``` html
+<w-tabs selected="finance">
+  <w-tabs-head>
+    <w-tabs-item name="sports"> 体育 </w-tabs-item>
+    <w-tabs-item name="finance"> 经济</w-tabs-item>
+    <w-tabs-item name="history"> 历史 </w-tabs-item>
+  </w-tabs-head>
+  <w-tabs-body>
+    <w-tabs-pane name="sports"> 体育新闻 </w-tabs-pane>
+    <w-tabs-pane name="finance"> 财经新闻 </w-tabs-pane>
+    <w-tabs-pane name="history"> 历史趣事 </w-tabs-pane>
+  </w-tabs-body>
+</w-tabs>
+```
