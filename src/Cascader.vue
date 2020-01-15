285f89f02b8e76ead9ed3f386827934d17c207f3
wushaolin <15265249516@163.com>
2020-01-15 22:53:42 +0800

    What: :zap: modify Cascader.vue
    Why:
    
    *
    
    How:
    
    *

--- src/Cascader.vue
+++ src/Cascader.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
-      <slot></slot>
-    </div>
-    <div class="popover">
-      <div v-for="(item, index) in source" :key="index">
-        <cascader-item :sourceItem="item"></cascader-item>
+    <div class="trigger" @click="handleTrigger"></div>
+    <div class="popover" v-if="popoverVisible">
+      <div class="level1" >
+        <div class="label" v-for="(item1, index) in source" :key="index" 
+          @click="level1Selected = item1">
+          {{ item1.name }}
+        </div>
+      </div>
+      <div class="level2">
+        <div class="label" v-for="(item2, index) in level2Items" :key="index" 
+          @click="level2Selected = item2">
+          {{ item2.name }}
+        </div>
+      </div>
+      <div class="level3">
+        <div class="label" v-for="(item3, index) in level3Items" :key="index">
+          {{ item3.name }}
+        </div>
       </div>
     </div>
   </div>
@@ -23,12 +35,56 @@ export default {
   components: {
     CascaderItem
   },
+  computed: {
+    level2Items() {
+      if (this.level1Selected) {
+        return this.level1Selected.children
+      } else {
+        return []
+      }
+    },
+    level3Items() {
+      if (this.level2Selected) {
+        return this.level2Selected.children
+      } else {
+        return []
+      }
+    },
+  },
+  data() {
+    return {
+      popoverVisible: false,
+      level1Selected: null,
+      level2Selected: null,
+    }
+  },
+  methods: {
+    handleTrigger() {
+      this.popoverVisible = !this.popoverVisible
+    }
+  },
 };
 </script>
 
 <style scoped lang="scss">
   @import "var";
   .cascader {
-
+    .trigger {
+      width: 100px;
+      height: 32px;
+      border: 1px solid red;
+    }
+    .popover {
+      // width: 80px;
+      height: 200px; 
+      display: flex;
+      border: 2px solid green;
+      .label {
+        white-space: nowrap;
+      }
+    }
+  }
+  .label {
+    border: 1px solid red;
   }
 </style>
