8e337c337118bb16b69b69f1afaacfa3e25b678c
wushaolin <15265249516@163.com>
2020-01-18 23:38:31 +0800

    What: add Popover.vue in vuepress
    Why:
    
    *
    
    How:
    
    *

---
+++ docs/.vuepress/components/Popover.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <w-popover>
+      
+    </w-popover>
+  </div>
+</template>
+
+<script>
+import Popover from '../../../src/Popover'
+export default {
+  props: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  watch: {
+
+  },
+  methods: {
+
+  },
+  components: {
+    "w-popover": Popover
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
